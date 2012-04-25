8d5dbe6966ca202f36f7a37da70905dae3b6baff
Ryan Merrill <ryan@helloample.com>
2012-04-24 20:42:54 -0400

    Adjust jQuery FitText

--- source/javascripts/rcm.js
+++ source/javascripts/rcm.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   prettyPrint();
   
-  $(".l-header hgroup h1").fitText(1.2, { minFontSize: '40px', maxFontSize: '46px' })
+  $(".l-header hgroup h1").fitText(1.2, { minFontSize: '38px', maxFontSize: '46px' })
 });
