f74a15638e402f8399e12cc382c34894adfaf293
procload <ryan@ryanmerrill.net>
2014-06-25 14:09:17 -0400

    Clean up config

--- config.rb
+++ config.rb
@@ -46,21 +46,18 @@ end
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   activate :cache_buster
-  
+
   # Use relative URLs
   activate :relative_assets
-  
+
   # Compress PNGs after build
-  # First: 
-  require "middleman-smusher"
-  activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # require "middleman-smusher"
+  # activate :smusher
+
 end
